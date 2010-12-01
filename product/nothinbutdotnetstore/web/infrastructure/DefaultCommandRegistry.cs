83134f3b43a0feecadebbdc636eea7e2178deeb0
nninja94 <mbogomolsky@gmail.com>
2010-12-01 14:03:07 -0800

    dev

--- product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
 
 namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        private IEnumerable<RequestCommand> all_commands;
+
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands)
+        {
+            this.all_commands = all_commands;
+        }
+
         public RequestCommand get_command_that_can_process(Request request)
         {
-            throw new NotImplementedException();
+            return this.all_commands.Where(x => x.can_process(request) == true).First();
         }
     }
 }
