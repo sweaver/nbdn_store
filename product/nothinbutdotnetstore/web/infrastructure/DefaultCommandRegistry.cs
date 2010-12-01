e8e67319a18d17eba335881ce3e627c045b569bd
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-12-01 14:58:32 -0700

    CommandRegistrySpecs

--- product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
-        
+        public RequestCommand get_command_that_can_process(Request request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
