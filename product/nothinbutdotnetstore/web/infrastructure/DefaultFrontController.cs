503adac51650513f81a9a4c15cdb1e98ef13a1f2
nninja94 <mbogomolsky@gmail.com>
2010-12-01 13:42:22 -0800

    dev

--- product/nothinbutdotnetstore/web/infrastructure/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultFrontController.cs
@@ -2,6 +2,17 @@ namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultFrontController : FrontController
     {
-        
+        private CommandRegistry command_registry;
+
+        public DefaultFrontController(CommandRegistry registry)
+        {
+            this.command_registry = registry;
+        }
+
+        public void process(Request request)
+        {
+            RequestCommand command = this.command_registry.get_command_that_can_process(request);
+            command.process(request);
+        }
     }
 }
