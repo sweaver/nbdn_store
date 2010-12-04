191362755f02b5452eb15ea2b29b454d26dc2520
unknown <Administrator@.(none)>
2010-12-03 17:30:33 -0700

    something

--- product/nothinbutdotnetstore/infrastructure/containers/basic/BasicDependencyBuilders.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/BasicDependencyBuilders.cs
@@ -14,7 +14,15 @@ namespace nothinbutdotnetstore.infrastructure.containers.basic
 
         public DependencyBuilder get_the_builder_to_build<Dependency>()
         {
-            return builders[typeof(Dependency)];
+            //if (builders[typeof(Dependency)] == null) throw new DependencyBuilderNotRegisteredException();
+            guard_against_a_builder_not_in_the_dictionary<Dependency>();
+            return builders[typeof (Dependency)];
+        }
+
+        private void guard_against_a_builder_not_in_the_dictionary<Dependency>()
+        {
+            if (!builders.ContainsKey(typeof(Dependency))) throw new DependencyBuilderNotRegisteredException();
+            return;
         }
     }
 }
